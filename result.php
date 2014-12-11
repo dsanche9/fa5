eee575d20eb521630a537c0d8e9cfb11916c9de0
dsanche9 <dsanche9@hawk.iit.edu>
2014-12-10 23:05:04 -0600

    fa5

---
+++ result.php
@@ -0,0 +1,56 @@
+
+<?php
+//result.php
+$_SESSION['email']=$_POST['email'];
+$_SESSION['phone']=$_POST['phone'];
+
+//$uploaddir = '/var/www/html/';
+$uploadfile = basename($_FILES['userfile']['name']);
+echo '<pre>';
+if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)){
+	echo "File successfully uploaded. \n";
+} else {
+	echo "error";
+}
+
+echo 'Here is some more debugging info:';
+print_r($_FILES);
+print "</pre>";
+
+
+require 'vendor/autoload.php';
+use Aws\S3\S3Client;
+
+//creates a client with my key and secret?
+$client = S3Client::factory();
+
+//create a bucket
+$bucket = uniqid("backend1", true);
+echo "Creating bucket named {$bucket}\n";
+$result = $client->createBucket(array(
+'Bucket' => $bucket
+));
+
+$client->waitUntilBucketExists(array('Bucket'=> $bucket));
+
+$key = $uploadfile;
+echo "Creating a new object with key {$key}\n";
+$result = $client->putObject(array(
+'ACL' => 'public-read',	
+'Bucket' => $bucket,
+'Key' => $key,
+'SourceFile' => $uploadfile
+));
+
+$url= $result['ObjectURL'];
+echo $_SESSION['email'] .PHP_EOL;
+echo $_SESSION['phone'] .PHP_EOL; 
+
+//hace lo de img blabla con un simple echo $url y luego 
+
+?>
+<img src="<?php echo $url; ?>" alt="Picture">
+
+
+
+
